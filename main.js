ccf93dfefe1aa1b7d6c806a6204ef9b6deb4a7d5
nisatamasumberjayapt-hub <nisatamasumberjayapt@gmail.com>
2025-11-09 15:29:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
  ****************************************************/
 
 // =================== KONFIGURASI ===================
-const API_URL = "https://script.google.com/macros/s/AKfycbyUUR6CxPVJsExI51dmuELVCswp9pIHoO_6aUc5GxqMQm_32AmDCh5IOKNbmu3z2hiS/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzMo11o1RsivZiGaEKW3agHIZUK8o3L1r7EulTx5e2Xne3U6fOMbl03Vkw4ku-ZGITW/exec";
 // GANTI jika perlu
 
 console.log("✅ main.js dimuat — PT ANISA JAYA UTAMA");
@@ -334,3 +334,4 @@ document.addEventListener("DOMContentLoaded", () => {
   if (btnLogin) btnLogin.addEventListener("click", (e) => { e.preventDefault(); login(); });
   if (btnRegister) btnRegister.addEventListener("click", (e) => { e.preventDefault(); register(); });
 });
+
